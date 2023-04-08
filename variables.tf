d3c306b0ab5e25f05fda97d7a84e6df37bd8fb99
Mark Cooper <mark.c.cooper@outlook.com>
2023-04-08 13:52:43 -0700

    Update vars defaults / desc

--- variables.tf
+++ variables.tf
@@ -7,9 +7,10 @@ variable "name" {
 }
 
 variable "slack_key" {
+  default     = ""
   description = "SSM param name for Slack webhook url"
 }
 
 variable "token_key" {
-  description = "SSM param name for token (used for authz)"
+  description = "SSM param name for token (used for basic authz)"
 }
